32b2173d3209673a50a91749944a26de61b6817e
Josh Dibble <joshdibble93@gmail.com>
2026-03-24 14:29:54 +0000

    speed insights and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Oswald, Archivo } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const oswald = Oswald({
@@ -125,6 +127,8 @@ export default function RootLayout({
           }}
         />
         {children}
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
